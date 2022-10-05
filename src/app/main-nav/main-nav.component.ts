ba8ea964cf5fe23dd7cd07647c9e93c01bf46852
Abhiram M <abhiramm913@gmail.com>
2022-10-05 15:22:56 +0530

    modified nav bar with hamburger menu

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavigationTabs } from '../app.enum';
 
@@ -7,11 +7,29 @@ import { NavigationTabs } from '../app.enum';
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent implements OnInit {
+export class MainNavComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('hamburger') hamburgerMenu: ElementRef<HTMLDivElement>;
+  @ViewChild('navMenu') navMenu: ElementRef<HTMLUListElement>;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.hamburgerMenu.nativeElement.addEventListener('click', () => {
+      this.toggleActiveClass();
+    });
+
+    this.navMenu.nativeElement.childNodes.forEach((li) => li.addEventListener('click', () => {
+      this.toggleActiveClass();
+    }));
+  }
+
+  private toggleActiveClass(): void {
+    this.hamburgerMenu.nativeElement.classList.toggle('active');
+    this.navMenu.nativeElement.classList.toggle('active');
   }
 
 }
